8f6e74458da3fe89176148c62b041ee6d9fceb32
Médérick Charles Fernand Grivel <grivel.mederick@gmail.com>
2022-12-06 19:09:34 -0500

    day 6

---
+++ 6/6.py
@@ -0,0 +1,49 @@
+def get_input(path):
+    with open(path, 'r') as f:
+        data = f.read()
+        return(data.strip())
+
+def part_one(file):
+    front = 0
+    back = 4
+    #index where num is found is always front+3
+    char_count = len(file)
+    for i in range(0, char_count):
+        if i<= char_count-4:
+            arr = []
+            for letter in file[front:back]:
+                arr.append(letter)
+            set_num = set(arr)
+            if len(set_num) == 4:
+                print(front+4)
+                break
+            front+=1
+            back+=1
+
+def part_two(file):
+    front = 0
+    back = 14
+    #index where num is found is always front+3
+    char_count = len(file)
+    for i in range(0, char_count):
+        if i<= char_count-4:
+            arr = []
+            for letter in file[front:back]:
+                arr.append(letter)
+            set_num = set(arr)
+            if len(set_num) == 14:
+                print(front+14)
+                break
+            front+=1
+            back+=1
+    
+
+
+AoC_input = get_input('6.txt')
+part_one(AoC_input)
+part_two(AoC_input)
+
+# v
+# n
+# c
+# z
